c7bd150ac3e247e25b0d7bfed9be1bd656276340
Darian Miller <darian@darianmiller.com>
2026-03-22 20:15:35 -0500

    Build tools + fix dunitx uses

---
+++ tools/Build.TokenDump.Win32.bat
@@ -0,0 +1,14 @@
+﻿@echo off
+setlocal
+
+call C:\Delphi\RADStudio37\bin\RSVars.bat
+
+set DPROJ=%~dp0..\projects\TokenDump\DelphiLexer.TokenDump.dproj
+
+msbuild "%DPROJ%" /p:Platform=Win32 /p:Config=Release /t:Build /verbosity:minimal
+if %ERRORLEVEL% NEQ 0 (
+    echo Build FAILED.
+    exit /b %ERRORLEVEL%
+)
+echo Build succeeded: ..\projects\TokenDump\Win32\Release\DelphiLexer.TokenDump.exe
+endlocal
---
+++ tools/Build_Run.Tests.Win32.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+setlocal
+
+call C:\Delphi\RADStudio37\bin\RSVars.bat
+
+set DPROJ=%~dp0..\test\DelphiLexer.Tests.dproj
+set EXE=%~dp0..\test\Win32\Debug\DelphiLexer.Tests.exe
+
+msbuild "%DPROJ%" /p:Platform=Win32 /p:Config=Debug /t:Build /verbosity:minimal /p:DCC_Define="$(DCC_Define);CI"
+if %ERRORLEVEL% NEQ 0 (
+    echo Build FAILED.
+    exit /b %ERRORLEVEL%
+)
+
+echo.
+"%EXE%"
+exit /b %ERRORLEVEL%
+endlocal
