fd4145d251be2d8380bbc97d3c23bd14cfb6c42f
Darian Miller <darian@darianmiller.com>
2026-05-17 11:11:57 -0500

    Minor cleanup for blog post

--- clean.bat
+++ clean.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal
+
 pushd "%~dp0"
 
-::
-:: Invoke-DelphiClean found bundled in: https://github.com/continuous-delphi/delphi-powershell-ci
-:: or stand-alone in: https://github.com/continuous-delphi/delphi-clean
-::
-pwsh -Command Invoke-DelphiClean -CleanLevel deep -CleanIncludeFilePattern "*.res"
+REM
+REM Invoke-DelphiClean found bundled in: https://github.com/continuous-delphi/delphi-powershell-ci
+REM or stand-alone in: https://github.com/continuous-delphi/delphi-clean
+REM
+
+pwsh -NoProfile -NoLogo -Command "$ErrorActionPreference='Stop'; Invoke-DelphiClean -CleanLevel deep -CleanIncludeFilePattern '*.res'"
 
 set "EXITCODE=%ERRORLEVEL%"
-
-:: if errorlevel 1 pause
-pause
-
+if %EXITCODE% NEQ 0 pause
 popd
+pause
 endlocal & exit /b %EXITCODE%
